ec15c11fb0fc22ea26e6d80cbd26244367257d8d
mbronshteyn <michael.bronshteyn@gmail.com>
2021-06-13 09:48:22 -0700

    add .gitignore and README.md add rest assured tests

---
+++ src/test/java/com/mbronshteyn/notification/controllers/NotificationControllerTest.java
@@ -0,0 +1,11 @@
+package com.mbronshteyn.notification.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+
+class NotificationControllerTest {
+
+}
