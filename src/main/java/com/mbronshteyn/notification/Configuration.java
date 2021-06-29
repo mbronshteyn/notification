d79b11635b439024003b2f88d4743eda9ce1a749
mbronshteyn <michael.bronshteyn@gmail.com>
2021-06-28 20:58:14 -0700

    integrate with template engine

--- src/main/java/com/mbronshteyn/notification/Configuration.java
+++ src/main/java/com/mbronshteyn/notification/Configuration.java
@@ -2,11 +2,21 @@ package com.mbronshteyn.notification;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Description;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
+
+//  @Bean
+//  public JavaMailSender mailSender() {
+//    // TODO: add more config if needed
+//    JavaMailSender mailSender = new JavaMailSenderImpl();
+//    return mailSender;
+//  }
 
 //  @Bean
 //  @Description("Thymeleaf Template Resolver")
--- src/main/java/com/mbronshteyn/notification/services/EmailService.java
+++ src/main/java/com/mbronshteyn/notification/services/EmailService.java
@@ -5,13 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Date;
 
 @Service
 @Slf4j
@@ -20,18 +25,36 @@ public class EmailService {
   @Value("${from.email.address}")
   private String fromEmailAddress;
 
-  @Autowired JavaMailSender mailSender;
+  @Autowired
+  JavaMailSender mailSender;
+
+  @Autowired
+  TemplateEngine templateEngine;
 
   @Async
   public void sendEmail(Notification notification)
       throws UnsupportedEncodingException, MessagingException {
+    MimeMessage message = getMimeMessage(notification);
+    log.info("Sending email from {}", fromEmailAddress);
+    mailSender.send(message);
+  }
+
+  private MimeMessage getMimeMessage(Notification notification) throws MessagingException {
     MimeMessage message = mailSender.createMimeMessage();
     MimeMessageHelper helper = new MimeMessageHelper(message);
     helper.setFrom(this.fromEmailAddress);
     helper.setTo(notification.getTo());
     helper.setSubject(notification.getTopic());
-    helper.setText(String.format("<p>%s</p>",notification.getMessageBody()), true);
-    log.info("Sending email from {}", fromEmailAddress);
-    mailSender.send(message);
+    helper.setText(String.format("<p>%s</p>", getHtmlContent(notification)), true);
+    return message;
+  }
+
+  private String getHtmlContent(Notification notification) {
+    final Context ctx = new Context();
+    ctx.setVariable("name", notification.getUser());
+    ctx.setVariable("today", new Date());
+    ctx.setVariable("fromEmail", fromEmailAddress);
+
+    return this.templateEngine.process("template.html", ctx);
   }
 }
